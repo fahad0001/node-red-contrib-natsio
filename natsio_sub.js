215ada07feb0f8d124a4dd125fd55a3d42cd0720
Fahad Mir <fahadmir0001@hotmail.com>
2021-05-03 06:01:42 +0500

    Refactor: Input for natsio_sub Refactor: Input Subject Array for dynamic subscribe to array of subjects

--- natsio_sub.js
+++ natsio_sub.js
@@ -6,22 +6,48 @@ module.exports = function(RED) {
 
     node.server = RED.nodes.getNode(n.server);
     node.server.setMaxListeners(node.server.getMaxListeners() + 1)
-    node.server.on('Status', (st) => { // (status,action)
-      if (st.text == 'connected') {
-        node.sid = node.server.nc.subscribe(n.subject,
+    node.sids = [];
+
+    node.on('input', (msg) => {
+      var subjects = msg.payload.subjects || [];
+      if(subjects.length > 0) {
+          subjects.forEach(sub => {
+            node.sids.push(node.server.nc.subscribe(sub,
+              {max: n.maxWanted,queue:n.queue},
+              (message, replyTo, subject) => {
+                node.send({payload: message, topic: subject, replyTo: replyTo});
+              }
+            ));
+          })
+          msg.payload.status = "New Subject List Updated";
+          node.send(msg);
+      } else {
+        node.sids.push(node.server.nc.subscribe(n.subject,
           {max: n.maxWanted,queue:n.queue},
           (message, replyTo, subject) => {
             node.send({payload: message, topic: subject, replyTo: replyTo});
           }
-        );
+        ));
+
+        msg.payload.status = "Used Default Subject";
+        msg.payload.subjects = n.subject
+        node.send(msg);
+      }
+    })
+
+    node.server.on('Status', (st) => { // (status,action)
+      if (st.text == 'connected') {
+        node.send("Default Subject: ", n.subject);
       }
       this.status(st)
     });
 
 
     node.on('close', () => {
-      if (node.sid) {
-        node.server.nc.unsubscribe(node.sid);
+      if (node.sids) {
+        node.sids.forEach(sid => {
+          node.server.nc.unsubscribe(sid);
+        });
       }
       node.server.setMaxListeners(node.server.getMaxListeners() - 1)
     });
