71426338c7f34c5c6c7177536898d8903ee10f85
Fahad Mir <fahadmir0001@hotmail.com>
2021-06-08 22:40:42 +0500

    Refactor: Subscriber Logs

--- natsio_sub.js
+++ natsio_sub.js
@@ -10,6 +10,7 @@ module.exports = function(RED) {
     node.globalSubjects = [];
 
     node.on('input', (msg) => {
+      console.log("Entry to Node on Input Event with Msg: ", msg);
       var subjects = msg.payload.subjects || [];
       subjects = subjects.reduce((acc, curr) => {
         if(!node.globalSubjects.some(sub => sub === curr)) {
@@ -19,36 +20,38 @@ module.exports = function(RED) {
       }, []);
 
       node.globalSubjects.push(...subjects);
-      msg.payload = {global: node.globalSubjects, subject: subjects};
+      msg.payload = {global: node.globalSubjects, subject: subjects, Logs: "Global: Subjects Added, msg Payload Added"};
+      console.log("Testing Instance for Payload: ", msg);
       node.send(msg);
       if(subjects.length > 0) {
-          subjects.forEach(sub => {
-            node.sids.push(node.server.nc.subscribe(sub,
-              {max: n.maxWanted,queue:n.queue},
-              (message, replyTo, subject) => {
-                node.send({payload: message, topic: subject, replyTo: replyTo});
-              }
-            ));
-          })
-          msg.payload.status = "New Subject List Updated";
-          node.send(msg);
-      } else if(node.globalSubjects.length <= 0) {
-        node.sids.push(node.server.nc.subscribe(n.subject,
-          {max: n.maxWanted,queue:n.queue},
-          (message, replyTo, subject) => {
-            node.send({payload: message, topic: subject, replyTo: replyTo});
-          }
-        ));
+        subjects.forEach(sub => {
+          console.log("Every Subject: ", sub)
+          node.sids.push(node.server.nc.subscribe(sub,
+            {max: n.maxWanted,queue: n.queue}, 
+            (message, replyTo, subject) => {
+              console.log("Final: ", message, " Topic: ", subject);
+              node.send({payload: message, topic: subject, replyTo: replyTo});
+            }
+          ));
+        });
+      }
+      else if(node.globalSubjects.length <= 0) {
+          node.sids.push(node.server.nc.subscribe(n.subject,
+            {max: n.maxWanted,queue:n.queue},
+            (message, replyTo, subject) => {
+              node.send({payload: message, topic: subject, replyTo: replyTo});
+            }
+          ));
 
-        msg.payload.status = "Used Default Subject";
-        msg.payload.subjects = n.subject
-        node.send(msg);
+          msg.payload.status = "Default Subject List";
+          msg.payload.subjects = n.subject
+          node.send(msg);
       }
     })
 
     node.server.on('Status', (st) => { // (status,action)
       if (st.text == 'connected') {
-        node.send("Default Subject: ", n.subject);
+        node.send("Initialized");
       }
       this.status(st)
     });
